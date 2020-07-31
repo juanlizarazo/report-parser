7e11e95772b97b9c16c31bc00c42f7b19b939fb8
Juan <gabriellg4@gmail.com>
2020-07-31 09:46:35 -0600

    promisify

--- src/app/app.js
+++ src/app/app.js
@@ -5,30 +5,42 @@ const path = require('path');
 const { ReportOutput } = require('./models');
 const { ReportParserHelper } = require('./helpers');
 
-const reportPath = path.resolve(__dirname, './report.txt');
-const readStream = fs.createReadStream(reportPath);
-const reportOutput = new ReportOutput();
-
-const readInterface = readline.createInterface({
-  input: readStream,
-  console: false
-});
-
-readInterface.on('line', function (line) {
-  console.log(`Parsing  ${line}`);
-
-  const parsedLine = ReportParserHelper.getTradeLine(line);
-
-  if (parsedLine) {
-    reportOutput.addTradeLine(parsedLine);
+class App {
+  constructor() {
+    this.reportOutput = new ReportOutput();
   }
-});
 
-readInterface.on('error', function (error) {
-  console.log(error);
-});
+  processReport() {
+    const reportPath = path.resolve(__dirname, './report.txt');
+    const readStream = fs.createReadStream(reportPath);
 
-readInterface.on('close', function () {
+    this.readInterface = readline.createInterface({
+      input: readStream,
+      console: false
+    });
+
+    this.readInterface.on('line', line => this._processLine(line));
+    this.readInterface.on('error', err => console.log(err));
+
+    return new Promise(resolve => {
+      this.readInterface.on('close', () => resolve(this.reportOutput));
+    });
+  }
+
+  _processLine(line) {
+    console.log(`Parsing  ${line}`);
+
+    const parsedLine = ReportParserHelper.getTradeLine(line);
+
+    if (parsedLine) {
+      this.reportOutput.addTradeLine(parsedLine);
+    }
+  }
+}
+
+const app = new App();
+
+app.processReport().then(reportOutput => {
   console.log('\n⭐️ Output:\n');
   console.log(JSON.stringify(reportOutput, null, 2));
 });
